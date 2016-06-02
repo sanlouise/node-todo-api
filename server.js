8abe3e011dc3128db83ebc17fda2577b92b6beaa
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-02 15:45:50 +0200

    Enable finding individual todos

--- server.js
+++ server.js
@@ -2,10 +2,46 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	description: 'Get groceries',
+	completed: false
+}, {
+	id: 2,
+	description: 'Work out',
+	completed: false
+}, {
+	id: 3,
+	description: 'Study',
+	completed: true
+}];
+
+//Homepage
 app.get('/', function (req, res) {
-
 	res.send('Todo API Root');
+});
 
+// Get all to-do items
+app.get('/todos', function (req, res) {
+	//Converts to json
+	res.json(todos);
+});
+
+//Get individual to-do's.   GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoId = req.params.id;
+	var matchedTodo;
+	todos.forEach(function(todo){
+		if (todoId === todo.id) {
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo)
+	} else res.status(404).send();
+	}
+	
 });
 
 app.listen(PORT, function () {
