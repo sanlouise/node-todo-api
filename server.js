ec2bfab362f041812ec49cdff3d5c2312cbc3c8b
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-04 18:38:57 +0200

    Add description query

--- server.js
+++ server.js
@@ -26,6 +26,12 @@ app.get('/todos', function (req, res) {
 		filteredTodos = _.where(filteredTodos, {completed: false});
 	}
 
+	if (queryParams,hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.indexOf(queryParams.q) > -1;
+		});
+	}
+
 	//Converts to json
 	res.json(filteredTodos);
 });
