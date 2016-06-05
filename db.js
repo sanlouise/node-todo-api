2c582b14d7040538aabb9415e6aca9c36df159b9
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-05 19:58:26 +0200

    Update environment variables for data storage

--- db.js
+++ db.js
@@ -1,9 +1,18 @@
 var Sequelize = require('sequelize');
-//Create a new instance of sequelize
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev-node-todos.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if (env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		'dialect': 'postgres',
+	});
+} else {
+
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-node-todos.sqlite'
+	});
+}
 
 var db = {};
 
