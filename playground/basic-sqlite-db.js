5351fb76ce683b6a27510553783b8c8cfb88010b
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-05 10:31:29 +0200

    Add basic sequelize validations

--- playground/basic-sqlite-db.js
+++ playground/basic-sqlite-db.js
@@ -1,19 +1,35 @@
 var Sequelize = require('sequelize');
+//Create a new instance of sequelize
 var sequelize = new Sequelize(undefined, undefined, undefined, {
 	'dialect': 'sqlite', 
-	'storage': 'basic-sqlite-db'
+	'storage': __dirname + '/basic-sqlite-db.sqlite'
 });
 
-//Create models 
+//Create Todo model
 var Todo = sequelize.define('todo', {
 	description: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			len: [1, 250]
+		}
 	}, 
 	completed: {
-		type: Sequelize.BOOLEAN
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false
+
 	}
-})
+});
 
 sequelize.sync().then(function () {
 	console.log('Everything is synched');
+
+	Todo.create({
+		description: 'Walk the dog',
+		completed: false
+	}).then(function (todo) {
+		console.log('Finished!');
+		console.log(todo);
+	});
 });
