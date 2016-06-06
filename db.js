877a70ad75eb71176310fd01f24832b66da0bb1c
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-06 13:34:52 +0200

    Update db

--- db.js
+++ db.js
@@ -4,22 +4,19 @@ var sequelize;
 
 if (env === 'production') {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
-		'dialect': 'postgres',
+		dialect: 'postgres'
 	});
 } else {
-
 	sequelize = new Sequelize(undefined, undefined, undefined, {
 		'dialect': 'sqlite',
-		'storage': __dirname + '/data/dev-node-todos.sqlite'
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
 	});
 }
 
 var db = {};
 
-//Loads in models from other files
 db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-//Exports db object to server.js
 module.exports = db;
