cd9126b0c365db5282ae7cad2889fe76be5e3f3f
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-06 15:18:10 +0200

    Install and require bcrypt in user model

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 // When imported with sequelize, ensure proper format of file.
 module.exports = function(sequelize, DataTypes) {
 
