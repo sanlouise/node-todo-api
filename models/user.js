a7d53dde4c0038767513841f6dae53220410e444
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-06 15:26:43 +0200

    Update user model for hashed passwords using salt

--- models/user.js
+++ models/user.js
@@ -13,11 +13,28 @@ module.exports = function(sequelize, DataTypes) {
 				isEmail: true
 			}
 		},
+		//In combination with bcrypt. Adds random set of chars before/after password before it is hashed.
+		salt: {
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		}, 
 		password: {
-			type: DataTypes.STRING,
+			//Virtual is not stored in db but is accessible.
+			type: DataTypes.VIRTUAL,
 			allowNull: false,
 			validate: {
 				len: [7, 100]
+			}, 
+			set: function (value) {
+				//Takes nr of chars you want to add to password.
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
+
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword);
 			}
 		}
 	}, {
