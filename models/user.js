7ab239e53e25ec518ea4c22faad21df0b85ddada
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-08 09:44:21 +0200

    Install jsonwebtoken

--- models/user.js
+++ models/user.js
@@ -5,80 +5,93 @@ var _ = require('underscore');
 module.exports = function(sequelize, DataTypes) {
 
 	var user = sequelize.define('user', {
-			email: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				unique: true,
-				validate: {
-					isEmail: true
-				}
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true
+			}
+		},
+		//In combination with bcrypt. Adds random set of chars before/after password before it is hashed.
+		salt: {
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
+		password: {
+			//Virtual is not stored in db but is accessible.
+			type: DataTypes.VIRTUAL,
+			allowNull: false,
+			validate: {
+				len: [7, 100]
 			},
-			//In combination with bcrypt. Adds random set of chars before/after password before it is hashed.
-			salt: {
-				type: DataTypes.STRING
-			},
-			password_hash: {
-				type: DataTypes.STRING
-			},
-			password: {
-				//Virtual is not stored in db but is accessible.
-				type: DataTypes.VIRTUAL,
-				allowNull: false,
-				validate: {
-					len: [7, 100]
-				},
-				set: function(value) {
-					//Takes nr of chars you want to add to password.
-					var salt = bcrypt.genSaltSync(10);
-					var hashedPassword = bcrypt.hashSync(value, salt);
+			set: function(value) {
+				//Takes nr of chars you want to add to password.
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
 
-					this.setDataValue('password', value);
-					this.setDataValue('salt', salt);
-					this.setDataValue('password_hash', hashedPassword);
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword);
+			}
+		}
+	}, {
+		hooks: {
+			// Runs before validate
+			beforeValidate: function(user, options) {
+				//Convert string emails to lowercase
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
 				}
 			}
-		}, {
-			hooks: {
-				// Runs before validate
-				beforeValidate: function(user, options) {
-					//Convert string emails to lowercase
-					if (typeof user.email === 'string') {
-						user.email = user.email.toLowerCase();
-					}
-				}
-			},
-			classMethods: {
-				authenticate: function(body) {
-					return new Promise(function(resolve, reject) {
+		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
 
-						if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							// Authentication failed
 							return reject();
 						}
-						user.findOne({
-							where: {
-								email: body.email
-							}
-						}).then(function(user) {
-							if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
-								// Authentication failed
-								return reject();
-							}
-							return resolve(user);
-						}, function(e) {
-							return reject();
-						});
+						return resolve(user);
+					}, function(e) {
+						return reject();
 					});
-				}
-
-			},
-			instanceMethods: {
-				toPublicJSON: function() {
-					var json = this.toJSON();
-					return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
-				}
+				});
 			}
 
-		});
+		},
+
+		//Use instance methods when working with existig models.
+		instanceMethods: {
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			},
+
+			generateToken: function (type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
+				try {
+					//Encrypt user information and create new JSON web token
+				} catch (e) {
+					return undefined;
+				}
+			}
+		}
+
+	});
 
 	return user;
 };
