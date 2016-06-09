6de66af2bac99079595f081745521caebb64c754
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-09 13:44:44 +0200

    Update middleware so that requests can only be made when token is stored in db

--- middleware.js
+++ middleware.js
@@ -1,16 +1,35 @@
+var cryptojs = require('crypto-js');
+
 module.exports = function (db) {
 	return {
 		//Without next, private code is never gonna run. This code is in server.js, whole block
 		//after 'app.get(/todos', . Middleware is run before that code executes.
 		requireAuthentication: function (req, res, next) {
-			var token = req.get('Auth');
+			var token = req.get('Auth') || '';
 
-			db.user.findByToken(token).then(function (user) {
+			//Looking for a token in the database, created via login
+			db.token.findOne({
+				where: {
+					//Looking for the hashed value
+					tokenHash: cryptojs.MD5(token).toString()
+				}
+			//If the tokenHash was found in db, run the text block of code
+			}).then(function (tokenInstance) {
+				if(!tokenInstance)
+					throw new Error();
+				}
+
+				req.token = tokenInstance;
+				rturn db.user.findByToken(token);
+
+				//If findByToken is successfull, this code will run
+			}).then (function(user) {
 				req.user = user;
 				next();
-			}, function () {
+
+			})catch(function () {
 				res.status(401).send();
-				//Next is not called, so private code is not run.
+
 			});
 		}
 	};
