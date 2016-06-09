1457c29f2a510ade4b3ec85f2efb59557273dbc2
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-09 13:47:51 +0200

    Add route to logout users

--- server.js
+++ server.js
@@ -183,6 +183,15 @@ app.post('/users/login', function(req, res) {
 	});
 });
 
+//DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+	req.token.destroy().then(function () {
+		res.status(204).send();
+	}).catch (function () {
+		res.status(500).send()
+	});
+});
+
 
 //Sync data to db
 db.sequelize.sync({force: true}).then(function() {
