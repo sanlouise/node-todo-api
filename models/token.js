1c883d755021b95b6e63f53d2b01d503cab96745
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-09 13:07:22 +0200

    Add token module to keep track of auth token from users

---
+++ models/token.js
@@ -0,0 +1,21 @@
+//We need to store the hashed value token to log out users.
+
+var cryptojs = require('crypto-js');
+
+module.exports = function (sequelize, DataTypes) {
+	return sequelize.define('token', {
+		token: {
+			type: DataTypes.VIRTUAL,
+			allowNull: false,
+			validate: {
+				len: [1]
+			}, 
+			set: function(value) {
+				var hash = cryptojs.MD5(value).toString();
+				this.setDataValue('token', value);
+				//Set tokenHash equal to hash
+				this.setDataValue('tokenHash', hash)
+			}
+		}
+	});
+};
